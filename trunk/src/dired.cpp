c9866b709f832df249b3fa44ff43efb7c09ef94d
elthariel <elthariel@c5f0c286-2d2a-0410-bbfd-f5f44cbd5b4e>
2007-02-24 17:12:36 +0000

    Some (other) ideas

--- trunk/src/dired.cpp
+++ trunk/src/dired.cpp
@@ -5,9 +5,42 @@
 // Login   <elthariel@epita.fr>
 //
 // Started on  Sat Feb 24 15:32:22 2007 Nahlwe
-// Last update Sat Feb 24 15:56:54 2007 Nahlwe
+// Last update Sat Feb 24 17:50:37 2007 Nahlwe
 //
+
+#include <sys/types.h>
+#include <dirent.h>
 
 #include "dired.hpp"
 
 using namespace std;
+
+Dired::Dired(std::string a_path)
+  : m_path(a_path)
+{
+  read_directory();
+}
+
+Dired::~Dired()
+{
+}
+
+void                    Dired::read_directory()
+{
+  DIR                   *dir;
+  struct dirent         *direl;
+
+  dir = opendir(m_path.c_str());
+  if (dir)
+    {
+      while (direl = readdir(dir))
+        {
+          m_dir.push_back(string(direl->d_name))
+;
+        }
+    }
+  else
+    {
+      //FIXME generate appropriate html error
+    }
+}
---
+++ trunk/src/http_res.cpp
@@ -0,0 +1,13 @@
+//
+// http_res.cpp for zia in /home/elthariel/code/zia/src
+//
+// Made by Nahlwe
+// Login   <elthariel@epita.fr>
+//
+// Started on  Sat Feb 24 17:51:16 2007 Nahlwe
+// Last update Sat Feb 24 18:11:57 2007 Nahlwe
+//
+
+#include "http_res.hpp"
+
+using namespace std;
