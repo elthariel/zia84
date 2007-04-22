87f8eb00790211628a05b30164e7303fcf48bf33
elthariel <elthariel@c5f0c286-2d2a-0410-bbfd-f5f44cbd5b4e>
2007-04-22 02:28:08 +0000

    Working perl embedded engine

--- src/psp/perlembed.cpp
+++ src/psp/perlembed.cpp
@@ -1,9 +1,9 @@
 
-
+#include <sys/mman.h>
 #include <iostream>
-//#include <cstdlib>
-//#include <cstdio>
-//#include "errno.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include "string.h"
 #include "perlembed.hpp"
 
@@ -13,23 +13,21 @@ using namespace std;
  * PerlEmbed class
  */
 
+const char              *PerlEmbed::m_perl_api = 0;
+int                     PerlEmbed::m_perl_api_fd = 0;
+
 PerlEmbed::PerlEmbed()
   : my_perl(0),
-    m_perl_initialized(false),
-    m_read_fd(0),
-    m_write_fd(0)
+    m_perl_initialized(false)
 {
   my_perl = perl_alloc();
   PL_perl_destruct_level = 1;
-  create_pipe();
 }
 
 PerlEmbed::~PerlEmbed()
 {
   perl_destruct(my_perl);
   perl_free(my_perl);
-  close(m_read_fd);
-  close(m_write_fd);
 }
 
 void                    PerlEmbed::init_perl()
@@ -44,9 +42,9 @@ void                    PerlEmbed::init_perl()
   perl_parse(my_perl, NULL, 3, perl_opts, NULL);
   perl_run(my_perl);
   m_perl_initialized = true;
-  inject_fd();
+  inject_api();
 }
-
+/*
 void                    PerlEmbed::create_pipe()
 {
   int                   fd[2];
@@ -81,6 +79,31 @@ void                    PerlEmbed::inject_fd()
   perl_eval_pv(cmd.c_str(), TRUE);
   cerr << "Injected" << endl;
 }
+*/
+
+void                    PerlEmbed::get_perl_api()
+{
+  struct stat           api_stat;
+
+  if (stat("zia_psp_api.pl", &api_stat) == -1)
+    cerr << "Unable to get file stats : " << strerror(errno) << endl;
+  else
+    if ((m_perl_api_fd = open("zia_psp_api.pl", O_RDONLY)) != -1)
+      m_perl_api = (const char *)mmap(0, api_stat.st_size,
+                                      PROT_READ, MAP_PRIVATE,
+                                      m_perl_api_fd, 0);
+    else
+      cerr << "Unable to mmap file : " << strerror(errno) << endl;
+
+}
+
+void                    PerlEmbed::inject_api()
+{
+  if (m_perl_api == 0)
+    get_perl_api();
+  if (m_perl_api != 0)
+    perl_eval_pv(m_perl_api, TRUE);
+};
 
 bool                    PerlEmbed::eval_perl(std::string &a_perl_expr)
 {
@@ -90,19 +113,18 @@ bool                    PerlEmbed::eval_perl(std::string &a_perl_expr)
 
 std::string             &PerlEmbed::get_out()
 {
-  char                  read_buf[1024];
-  string                *out = new string();
-  int                   read_size;
+  string                *out = new string;
+  SV                    *perl_string;
+  char                  *real_perl_string;
+  STRLEN                real_perl_string_len;
 
-  while((read_size = read(m_read_fd, (char *)&read_buf, 1024)) == 1024)
+  perl_string = perl_get_sv("main::__psp_out_buffer_magic42", FALSE);
+  if (perl_string != NULL)
     {
-      out->append((char *)&read_buf, read_size);
+      real_perl_string = SvPV(perl_string, real_perl_string_len);
+      out->append(real_perl_string, real_perl_string_len);
     }
-  /*  read_size = read(m_read_fd, (char *)&read_buf, 1);
-  read_buf[1] = 0;
-  cout << "read this : " << read_buf << endl;*/
-  out->append((char *)&read_buf, read_size);
-  return (*out);
+  return *out;
 }
 
 /*
@@ -112,11 +134,10 @@ std::string             &PerlEmbed::get_out()
 int main(int ac, char **av)
 {
   PerlEmbed p;
-  string test("print \"hello world !\\n\n\n\n\n\n\n\";");
+  string test("echo \"hello world !\\n\";");
   p.init_perl();
   cerr << "Perl init" << endl;
-  for (int i = 0; i < 5000; i++)
-    p.eval_perl(test);
-  cerr << "Command evaluated" << endl;
+  p.eval_perl(test);
+  cerr << "Command evaluated : " << endl << test << endl;
   cout << p.get_out() << endl;
 }
