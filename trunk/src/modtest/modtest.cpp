9a1b17f458f5c62cad0fe74e400be22acef29671
elthariel <elthariel@c5f0c286-2d2a-0410-bbfd-f5f44cbd5b4e>
2007-04-23 05:32:38 +0000

    dsl pour la merde ds le configure

---
+++ trunk/src/modtest/modtest.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include "modtest.hh"
+#include <EZ_IStreamBuffer.h>
+
+using namespace std;
+
+TestMod::TestMod(unsigned int a_id)
+  : m_id(a_id),
+    m_next_mod(this)
+{
+  cerr << "Create TestMod #" << a_id << endl;
+}
+
+bool                    TestMod::needProceed()
+{
+  return (true);
+}
+
+bool                    TestMod::proceed()
+{
+  // FIXME
+  return (true);
+}
+
+const EZ_IModule&       TestMod::getNext() const
+{
+  return (*m_next_mod);
+}
+
+bool                    TestMod::setNext(EZ_IModule &a_mod)
+{
+  m_next_mod = &a_mod;
+  return (true);
+}
+
+EZ_IBuffer              *TestMod::popBuffer()
+{
+  EZ_IBuffer            *buf;
+
+  if (m_bufs.size() > 0)
+    {
+      buf = *m_bufs.begin();
+      m_bufs.pop_front();
+      return buf;
+    }
+  else
+    return 0;
+}
+
+bool                    TestMod::pushBuffer(EZ_IBuffer &a_buf)
+{
+  m_bufs.push_back(&a_buf);
+  return (true);
+}
+
+std::string             TestMod::getModuleName() const
+{
+  return ("Zia TestMod");
+}
+
+std::string             TestMod::getModuleDescription() const
+{
+  return ("Zia TestMod : outputs infos on the buffer which pass through itself");
+}
+
+EZ_IModule::ModuleLevel TestMod::getModuleLevel() const
+{
+  return (EZ_IModule::EZ_IN);
+}
+
+void                    TestMod::setServerConfiguration(const std::string&, const std::string&)
+{
+}
+
+void                    TestMod::clean(void)
+{
+  for(buflist::iterator i = m_bufs.begin();
+       i != m_bufs.end(); i++)
+    delete *i;
+  m_bufs.clear();
+}
+
+TestMod::TestMod()
+{
+}
+
+/*
+ * Module c api
+ */
+EXPORT EZ_IModule	*getInstance()
+{
+  static unsigned int   instance_id= 0;
+
+  return (new TestMod(instance_id++));
+}
---
+++ trunk/src/modtest/modtest.hh
@@ -0,0 +1,62 @@
+/*
+** modtest.hh
+** Login : <lta@lta-laptop>
+** Started on  Sat Apr 21 22:57:51 2007 eltha
+** $Id$
+**
+** Copyright (C) 2007 eltha
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+#ifndef   	MODTEST_HH_
+# define   	MODTEST_HH_
+
+#include <list>
+#include <EZ_IModule.h>
+
+class TestMod : public EZ_IModule
+{
+public:
+  typedef std::list<EZ_IBuffer *> buflist;
+
+  TestMod(unsigned int a_id);
+
+  virtual bool                    needProceed();
+  virtual bool                    proceed();
+
+  virtual const EZ_IModule&       getNext() const;
+  virtual bool                    setNext(EZ_IModule &);
+
+  virtual EZ_IBuffer              *popBuffer();
+  virtual bool                    pushBuffer(EZ_IBuffer &);
+
+  virtual std::string             getModuleName() const;
+  virtual std::string             getModuleDescription() const;
+  virtual EZ_IModule::ModuleLevel getModuleLevel() const;
+
+  virtual void                    setServerConfiguration(const std::string&, const std::string&);
+
+  virtual void                    clean(void);
+
+protected:
+  TestMod();
+
+  unsigned int          m_id;
+  EZ_IModule            *m_next_mod;
+  buflist               m_bufs;
+
+};
+
+#endif	    /* !MODTEST_HH_ */
